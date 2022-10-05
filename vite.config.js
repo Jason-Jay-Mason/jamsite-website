05ab0d5a80e2d3706f327d381a3d5521b13d0c97
Jason Mason <jasonjaymason@gmail.com>
2022-10-05 12:31:19 -0600

    Removed party town for now

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-// import path from 'path';
-// import { partytownVite } from '@builder.io/partytown/utils';
 
 const config = {
 	plugins: [
 		sveltekit(),
-		// partytownVite({
-  //       dest: path.join(__dirname, 'static', '~partytown'),
-		// })
 	]
 };
 
